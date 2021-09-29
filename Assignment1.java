8ee5b9a7895775b746a74a35b13d00d884b59648
Hani M. Th <hmthubaiti@uqu.edu.sa>
2021-09-29 23:54:19 +0300

    try to solve the program

--- Assignment1.java
+++ Assignment1.java
@@ -1,6 +1,9 @@
 import java.util.scanner;
 //Your Name Amro Hinadi
 //Your ID  442009861
+//your grade is 0/5
+//@ line 1 : scanner should be Scanner in uppercase
+//@ Class should be class in lowercase
 Class Main {
  public static void main(string[] args {
    //start your code
